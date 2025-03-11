c6e7e9940e688741ca80ccba3ba593186cee2330
charith0901 <charithjayasankha@gmail.com>
2025-03-12 01:34:34 +0530

    Create ecosystem.config.js

---
+++ ecosystem.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    apps: [{
+      name: "team-portfolio",
+      script: "npm",
+      args: "run start",
+      cwd: "/var/www/team-portfolio",
+      watch: true,
+    }]
+  };
+  
