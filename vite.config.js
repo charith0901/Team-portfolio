911a19dfefd85319bb604f8f6e480f0ef61a9181
charith0901 <charithjayasankha@gmail.com>
2025-03-16 11:16:13 +0530

    sitemap

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,25 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import createSitemap from 'vite-plugin-sitemap'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),
-    tailwindcss()
+    tailwindcss(),
+    createSitemap({
+      hostname: 'https://datagragons.live',
+      lastmod: new Date().toISOString().slice(0, 10),
+      changefreq: 'weekly',
+      priority:1,  
+      dynamicRoutes: [
+          '/member/charith_dev',
+          '/member/gimhana_dev',
+          '/member/sameera_dev',
+          '/member/janith_dev',
+          '/project/Ruhuna ScheduleEase'
+        ],
+      generateRobotsTxt: true, // This generates robots.txt
+    })
   ],
 })
